2120f3675bec8acabd92286cf3b8364b848b94eb
ambarish2012 <ambarish@shippable.com>
2017-11-03 13:32:53 -0700

    update sample

--- provision_gke_cluster.sh
+++ provision_gke_cluster.sh
@@ -1,4 +1,12 @@
 # This script provisions a GKE cluster
 #! /bin/bash -e
+response=$(gcloud container clusters describe $1 --zone $2 || echo "ClusterNotFound")
+echo "cluster query response: "$response
 
-gcloud container clusters create $1 --num-nodes=1 --machine-type=n1-standard-1
+if [[ $response = "ClusterNotFound" ]]
+then
+   echo "cluster not found, creating cluster"
+   gcloud container clusters create $1 --num-nodes=1 --machine-type=n1-standard-1
+else
+   echo "cluster already exists, skipping creating cluster"
+fi
